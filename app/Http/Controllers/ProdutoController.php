7f9859cac34230ecca6946e551650f2c690af5a5
Marcos Pereira <marcosprr34@gmail.com>
2019-02-05 18:01:55 -0300

    function lista()

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -5,8 +5,17 @@ use Illuminate\Support\Facades\DB;
 
 Class ProdutoController extends Controller {
     public function lista(){
+        $html = '<h1> Lista de produtos</h1>';
+
+        $html .= '<ul>';
+
         $produtos = DB::select('select * from produtos');
 
-        return '<h1>Lista de produtos</h1>';
+        foreach($produtos as $p) {
+            $html .= '<li>nome: '. $p ->nome .' 
+                        descricao :'. $p -> descricao .'</li>' ;
+        }
+        $html .= '</ul>';
+        return $html;
     }
 }
