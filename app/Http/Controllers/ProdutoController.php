8b685d45bcedfdc6ee24768cdda136c52e42c84e
Marcos Pereira <marcosprr34@gmail.com>
2019-02-06 16:50:48 -0300

    produtocontroller logic moved to views/listagem

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -5,17 +5,9 @@ use Illuminate\Support\Facades\DB;
 
 Class ProdutoController extends Controller {
     public function lista(){
-        $html = '<h1> Lista de produtos</h1>';
-
-        $html .= '<ul>';
 
         $produtos = DB::select('select * from produtos');
+        return view('listagem')->with('produtos',$produtos);
 
-        foreach($produtos as $p) {
-            $html .= '<li>nome: '. $p ->nome .' 
-                        descricao :'. $p -> descricao .'</li>' ;
-        }
-        $html .= '</ul>';
-        return $html;
     }
 }
