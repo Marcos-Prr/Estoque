d80b07797e49e71da2971036464dc4135f56d5b9
Marcos Pereira <marcosprr34@gmail.com>
2019-02-06 16:51:41 -0300

    insert data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Model::unguard();
+        $this->call('ProdutoTableSeeder');
         // $this->call(UsersTableSeeder::class);
+    }
+}
+
+class ProdutoTableSeeder extends Seeder {
+    public function run()
+    {
+        DB::insert('insert into produtos (nome,quantidade,valor,descricao)
+        values(?,?,?,?)',array('geladeira',2,50,'side by side com gelo'));
+        
+        DB::insert('insert into produtos (nome,quantidade,valor,descricao)
+        values(?,?,?,?)',array('fogao',5,100,'automatico'));
+        
+        DB::insert('insert into produtos (nome,quantidade,valor,descricao)
+        values(?,?,?,?)',array('microondas',1,150,'esquenta tudo'));
+        
+
     }
 }
